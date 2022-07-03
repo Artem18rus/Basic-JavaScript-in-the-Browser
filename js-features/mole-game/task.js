6f270eb6c7e91f61f17e8168cb7bb138495fea9d
Artem18 <urart88@mail.ru>
2022-07-03 21:29:09 +0400

    DZ1 на проверку v2

--- js-features/mole-game/task.js
+++ js-features/mole-game/task.js
@@ -1,17 +1,11 @@
 const value = document.querySelectorAll('.hole');
 console.log(value);
-//const value2 = document.getElementsByClassName('hole hole_has-mole');
-//console.log(value2);
 const dead = document.getElementById('dead');
 const lost = document.getElementById('lost');
 //console.log(lost);
 
 for(let index = 0; index < 9; index++) {
-//console.log(value[index]);
-//console.log(dead.textContent++)
-
   value[index].onclick = function() {
-    //console.log(value[index].classList.contains('hole_has-mole'));
     if(value[index].classList.contains('hole_has-mole')) {
       dead.textContent++;
     } else {
