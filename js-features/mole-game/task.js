2e17f190cb686f40cdf40ddeedaad0d5f8dd847d
Artem18 <urart88@mail.ru>
2022-07-03 21:39:26 +0400

    DZ1 на проверку v2

--- js-features/mole-game/task.js
+++ js-features/mole-game/task.js
@@ -13,9 +13,11 @@ for(let index = 0; index < 9; index++) {
 
     if(dead.textContent == 10) {
       dead.textContent = 0;
+      lost.textContent = 0;
       alert('Вы выиграли');
       
     } else if(lost.textContent == 5) {
+      dead.textContent = 0;
       lost.textContent = 0;
       alert('Вы проиграли');
     }
