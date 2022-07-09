080f0d6ea54b7c987f918ed9543c387c17edb8a0
Artem18 <urart88@mail.ru>
2022-07-09 14:48:01 +0400

    DZ2(element-search) на проверку v1

--- element-search/popups/task.js
+++ element-search/popups/task.js
@@ -4,15 +4,22 @@ modal_main.style.display = 'flex';
 const modal_active = document.querySelector('.modal_active');
 modal_active.style.display = 'none';
 
-const modal__close = document.querySelector('.modal__close');
-modal__close.onclick = function() {
-	modal_main.style.display = 'none';
+const modal__close = document.querySelectorAll('.modal__close');
+//console.log(modal__close);
+
+for(let i = 0; i < modal__close.length; i++) {
+  if(i == 1) continue;
+  modal__close[i].onclick = function() {
+    modal_main.style.display = 'none';
+    modal_success.style.display = 'none';
+  }  
 }
 
 const modal_success = document.querySelector('#modal_success');
-console.log(modal_success);
+//console.log(modal_success);
 const show_success = document.querySelector('.show-success');
 
 show_success.onclick = function() {
  	modal_success.style.display = 'flex';
 }
+
