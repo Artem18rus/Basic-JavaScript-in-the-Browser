1e19d0730446715a16868f90df48f288edf47eba
Artem18 <urart88@mail.ru>
2022-07-10 18:27:23 +0400

    DZ2(element-search) на проверку v2

--- element-search/slider/task.js
+++ element-search/slider/task.js
@@ -6,13 +6,13 @@ const sliderArrowNext = document.querySelector('.slider__arrow_next');
 sliderArrow.forEach((item) => {
   item.addEventListener('click', (event) => {
     if(item.classList.contains('slider__arrow_prev')) {
-      let activeIndex = sliderItem.findIndex((value, idx) => value.classList.contains('slider__item_active'));
+      let activeIndex = sliderItem.findIndex((value) => value.classList.contains('slider__item_active'));
       
       sliderItem[activeIndex].classList.remove('slider__item_active');        
       activeIndex = (activeIndex == 0) ? (sliderItem.length - 1) : (activeIndex - 1);
       sliderItem[activeIndex].classList.add('slider__item_active');
     } else {
-      let activeIndex = sliderItem.findIndex((value, idx) => value.classList.contains('slider__item_active'));
+      let activeIndex = sliderItem.findIndex((value) => value.classList.contains('slider__item_active'));
       
       sliderItem[activeIndex].classList.remove('slider__item_active');        
       activeIndex = (activeIndex == sliderItem.length - 1) ? 0 : (activeIndex + 1);
