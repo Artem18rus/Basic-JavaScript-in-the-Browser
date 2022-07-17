0a9b77feba468a4108c9430a38526ee3a6d6a26b
Artem18 <urart88@mail.ru>
2022-07-17 15:06:12 +0400

    DZ4(DOM) на проверку v2

--- dom/reveal/task.js
+++ dom/reveal/task.js
@@ -2,8 +2,8 @@ const reveal = document.querySelectorAll('.reveal');
 
 reveal.forEach((item) => {
   window.addEventListener('scroll', (event) => {
-  if(window.innerHeight > item.getBoundingClientRect().top) {
-    item.classList.add('reveal_active');
-  }
+    if(window.innerHeight > item.getBoundingClientRect().top) {
+      item.classList.add('reveal_active');
+    }
   })
 })
